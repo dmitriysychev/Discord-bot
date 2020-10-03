e7dd3017c9e43e9d8df47f0dc24dd990b8792e1f
Dmitriy Sychev <44623981+dmitriysychev@users.noreply.github.com>
2020-10-02 22:38:49 -0500

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -4,34 +4,6 @@ module.exports = {
     name: '.ебашь',
     async execute(message, args) {
         console.log('The function has been moved to index.js');
-        /*
-        const argse = message.content.split(" ");
-        if (argse.length < 2) {
-            message.channel.send("Ты не указал песню уебан");
-        } else {
-            const connection = await message.member.voice.channel.join();
-            console.log(connection.speaking.bitfield);
-            //если бот уже играет песню
-            if (connection.speaking.bitfield) {
-                let url = argse[1];
-                console.log('url to queue', url);
-                musicQueue.push(url);
-            } else {
-                console.log(`Очередь пустая? ${musicQueue.isEmpty()}`);
-                musicDispatcher = await connection.play(ytdl(argse[1], { quality: 'highestaudio', volume: 0.4}));
-                musicDispatcher.on('finish', async () => {
-                    if (!musicQueue.isEmpty()){
-                        let toPlay = musicQueue.poll();
-                        console.log('toPlay\n', toPlay);
-                        connection.play(ytdl(toPlay,  { quality: 'highestaudio', volume: 0.4}));
-                    } else {
-                        message.channel.send("Уебанчики, музло кончилось");
-                    }
-                    
-                })
-            }
-        }
-        */
         },
   };
             
